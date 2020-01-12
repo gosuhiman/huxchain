a77d1ea33906bf644e3e68a57ee1a926cdf3af6d
gosuhiman <gosuhiman@gmail.com>
2020-01-12 21:56:54 +0100

    Add registerNode and resolveConflicts blockchain methods

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,8 +1,10 @@
+import axios from "axios";
 import crypto from "crypto";
 import uuid from "uuid";
 
 import {Block} from "./block";
 import {Transaction} from "./transaction";
+import {GetChainResponseDto} from "./dtos/get-chain.dto";
 
 export class Blockchain {
   public static hash(block: Block): string {
@@ -20,6 +22,23 @@ export class Blockchain {
     return guess.substr(0, 4) === "0000";
   }
 
+  public static validChain(chain: Block[]): boolean {
+    if (chain.length === 0)  return false;
+
+    let lastBlock: Block = chain[0];
+    let currentIndex = 1;
+
+    while (currentIndex < chain.length) {
+      const block = chain[currentIndex];
+      if (block.previousHash !== Blockchain.hash(lastBlock)) return false;
+      if (!Blockchain.validProof(lastBlock.proof, block.proof)) return false;
+      lastBlock = block;
+      currentIndex++;
+    }
+
+    return true;
+  }
+
   public static generateAddress(): string {
     return uuid.v4().replace("-", "");
   }
@@ -27,6 +46,7 @@ export class Blockchain {
   public nodeAddress: string = Blockchain.generateAddress();
   public chain: Block[] = [];
   private currentTransactions: Transaction[] = [];
+  private nodes: Set<string> = new Set();
 
   constructor() {
     this.newBlock(100, "1");
@@ -40,8 +60,10 @@ export class Blockchain {
       proof,
       previousHash: previousHash || Blockchain.hash(this.lastBlock()),
     };
+
     this.currentTransactions = [];
     this.chain.push(block);
+
     return block;
   }
 
@@ -52,12 +74,45 @@ export class Blockchain {
 
   public mine(): Block {
     const proof: number = Blockchain.proofOfWork(this.lastBlock().proof);
+
     this.newTransaction({
       sender: "0",
       recipient: this.nodeAddress,
       amount: 1,
     });
+
     return this.newBlock(proof);
+  }
+
+  public registerNode(url: string) {
+    const parsedUrl = new URL(url);
+    this.nodes.add(parsedUrl.host);
+  }
+
+  // Consensus Algorithm - longest valid chain wins
+  public async resolveConflicts() {
+    const neighbours: Set<string> = this.nodes;
+    let newChain: Block[] | undefined;
+    let maxLength: number = this.chain.length;
+
+    for (const node of neighbours) {
+      const response = await axios.request<GetChainResponseDto>({url: `http://${node}/chain`});
+      if (response.status !== 200) continue;
+      const chain: Block[] = response.data.chain;
+      const length: number = response.data.length;
+
+      if (length > maxLength && Blockchain.validChain(chain)) {
+        maxLength = length;
+        newChain = chain;
+      }
+    }
+
+    if (newChain) {
+      this.chain = newChain;
+      return true;
+    }
+
+    return false;
   }
 
   private lastBlock(): Block {
